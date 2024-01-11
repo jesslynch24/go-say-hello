afa5d7379056cfab97f4d9ab77227e158bfc7e09
Jesslyn CH <jech@ubslinux.com>
2024-01-11 15:08:40 +0700

    update dependency

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,19 @@
 package gosayhello
 
 func SayHello() string {
-	return "hello"
+	return "hello world"
 }
+
+//create git
+//git init
+//git add .
+//git commit -m "first commit"
+//git remote add origin https://github.com/jesslynch24/go-say-hello.git
+//git push -u origin master
+//git tag v1.0.0
+//git push origin v1.0.0
+
+//open app_say_hello
+//create git
+//go mod init github.com/jesslynch24/app-say-hello
+//go get github.com/jesslynch24/go-say-hello
